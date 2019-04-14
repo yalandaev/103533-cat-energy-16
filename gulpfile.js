b004ade1dd721ebf0dc9368ec74f3b3b75858e5c
Eugene Yalandaev <yalandaev@gmail.com>
2019-04-15 02:50:01 +0400

    Верстка главной страницы (без уточнения отступов) + svg спрайты

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,35 @@ var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var svgSprite = require('gulp-svg-sprite');
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
-    .pipe(sass())
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths
+    }))
     .pipe(postcss([
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task('svgsprite', function () {
+  return gulp.src('source/img/*.svg') // svg files for sprite
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../sprite.svg"  //sprite file name
+        }
+      },
+    }
+    ))
+    .pipe(gulp.dest('source/img/'));
 });
 
 gulp.task("server", function () {
@@ -34,4 +50,4 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("start", gulp.series("css", "svgsprite", "server"));
